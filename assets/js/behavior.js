f7996374f9121d5bd8986bab982f278d2c21ecba
Devin Alexander <alexanderdev117@gmail.com>
2019-10-21 14:37:51 -0500

    more work on user inputs

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,118 +1,144 @@
 var uppercase;
-var upperanswer;
+var upperanswer = null;
 var lowercase;
-var loweranswer;
+var loweranswer = null;
 var numeric;
-var numericanswer;
+var numericanswer = null;
 var specialchar;
-var specialanswer;
-var length;
-var lengthanswer;
+var specialanswer = null;
+var length = 0;
 
 // Opening Alert for the user.
 alert("Welcome to my password generator! You will be asked to choose from the following password criteria:")
 
-// Asking for the use of upper-case letters in the password.
-while (uppercase !== "yes" || uppercase !== "no") {
-   
-    uppercase = prompt("Do you want upper-case characters? (Please answer yes or no)");
-    
-    if (uppercase === "yes") {
-        console.log(uppercase);
-        upperanswer = true;
-        console.log(upperanswer);
-    }
-    
-    else if (uppercase === "no") {
-        console.log(uppercase);
-        upperanswer = false;
-        console.log(upperanswer);
-    }
+while (upperanswer === null && loweranswer === null && numericanswer === null && specialanswer === null ) {
 
-    else {
-        alert("Please enter a valid answer.")
+    
+// Asking for the use of upper-case letters in the password.
+    while (uppercase !== "yes" && uppercase !== "no") {
+   
+        uppercase = prompt("Do you want upper-case characters? (Please answer yes or no)");
+    
+        if (uppercase === "yes") {
+            upperanswer = true;
+            console.log(upperanswer);
+        }
+    
+        else if (uppercase === "no") {
+            upperanswer = false;
+            console.log(upperanswer);
+        }
+
+        else {
+            alert("Please enter a valid answer.")
+        }
     }
-}
 
 // Asking for the use of lower-case letters in the password.
-while (lowercase !== "yes" || lowercase !== "no") {
+        while (lowercase !== "yes" && lowercase !== "no") {
    
-    lowercase = prompt("Do you want lower-case characters? (Please answer yes or no)");
+        lowercase = prompt("Do you want lower-case characters? (Please answer yes or no)");
     
-    if (lowercase === "yes") {
-        console.log(lowercase);
-        loweranswer = true;
-        console.log(loweranswer);
-    }
+        if (lowercase === "yes") {
+            loweranswer = true;
+            console.log(loweranswer);
+        }
     
-    else if (lowercase === "no") {
-        console.log(lowercase);
-        loweranswer = false;
-        console.log(loweranswer);
-    }
+        else if (lowercase === "no") {
+            loweranswer = false;
+            console.log(loweranswer);
+        }
 
-    else {
-        alert("Please enter a valid answer.")
+        else {
+            alert("Please enter a valid answer.")
+        }
     }
-}
 
 // Asking for the use of numbers in the password.
-while (numeric !== "yes" || numeric !== "no") {
+    while (numeric !== "yes" && numeric !== "no") {
    
-    numeric = prompt("Do you want numeric characters? (Please answer yes or no)");
+        numeric = prompt("Do you want numeric characters? (Please answer yes or no)");
     
-    if (numeric === "yes") {
-        console.log(numeric);
-        numericanswer = true;
-        console.log(numericanswer);
-    }
+        if (numeric === "yes") {
+            numericanswer = true;
+            console.log(numericanswer);
+        }
     
-    else if (numeric === "no") {
-        console.log(numeric);
-        numericanswer = false;
-        console.log(numericanswer);
-    }
+        else if (numeric === "no") {
+            numericanswer = false;
+            console.log(numericanswer);
+        }
 
     else {
         alert("Please enter a valid answer.")
+        }
     }
-}
 
 // Asking for the use of special characters in the password.
-while (specialchar !== "yes" || specialchar !== "no") {
+    while (specialchar !== "yes" && specialchar !== "no") {
    
-    specialchar = prompt("Do you want special characters? (Please answer yes or no)");
+        specialchar = prompt("Do you want special characters? (Please answer yes or no)");
     
-    if (specialchar === "yes") {
-        console.log(specialchar);
-        specialanswer = true;
-        console.log(specialanswer);
-    }
+        if (specialchar === "yes") {
+            specialanswer = true;
+            console.log(specialanswer);
+        }
     
-    else if (specialchar === "no") {
-        console.log(specialchar);
-        specialanswer = false;
-        console.log(specialanswer);
+        else if (specialchar === "no") {
+            specialanswer = false;
+            console.log(specialanswer);
+        }
+
+        else {
+            alert("Please enter a valid answer.")
+        }
     }
 
-    else {
-        alert("Please enter a valid answer.")
-    }
+// Determine the length of the password.
+    while (length > 128 || length < 8 || typeof length === "string") {
+   
+        length = prompt("How long do you want this password to be? (Must be between 8 and 128 characters)");
+        
+    
+        if (length < 128 && length > 8) {
+            alert("Your password will be " + length + " characters long.")
+            console.log(length);
+            console.log(typeof length);
+        }
+
+        else {
+            alert("Your password must be between 8 and 128 characters and be a number.")
+            console.log(typeof length);
+        }
 }
 
-// Asking for the specific length the user wants the password to be.
-while (length > 128 && length < 8 && typeof length !== "number") {
-   
-    length = prompt("How long do you want this password to be? (Must be between 8 and 128 characters)");
-    length = Number(linkanswer);
+// Keeps the user from inputing all criteria as false.
+if (upperanswer === false && loweranswer === false && numericanswer === false && specialanswer === false){
+    uppercase = null;
+    upperanswer = null;
+    lowercase = null;
+    loweranswer = null;
+    numeric = null;
+    numericanswer = null;
+    specialchar = null;
+    specialanswer = null;
+    length = 0;
+    alert("You must choose at least one of the criteria. Please start again.");
     
-    if (length < 128 && length && typeof link === "number" > 8) {
-        length = Number(linkanswer);
-        alert("Your password will be " + lengthanswer + " characters long.")
-    }
-
-    else {
-        alert("Your password must be between 8 and 128 characters and be a number.")
-    }
 }
+else {
+    alert("Thank you.");
+}
+
+}
+
+// ASK HOW TO CONVERT DATA TYPE
+// ASK WHERE TO PUT IN .toLowercase
+
+
+
+
+      
+
+
 
